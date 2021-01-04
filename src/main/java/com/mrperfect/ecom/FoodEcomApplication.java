0ad84119204f7c52786b591e9514de41d11fe5e4
Mr.Perfect <suvammohapatra8888@gmail.com>
2021-01-04 08:07:21 +0530

    first commit

---
+++ src/main/java/com/mrperfect/ecom/FoodEcomApplication.java
@@ -0,0 +1,13 @@
+package com.mrperfect.ecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodEcomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodEcomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mrperfect/ecom/FoodEcomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrperfect.ecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodEcomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
